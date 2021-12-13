3eb52e7ab41b415a344ddea009dea5d924e1469b
Johannes Tykesson <johannes.tykesson@tietoevry.com>
2021-12-13 21:28:09 +0100

    Initial commit

---
+++ ConsoleApp/ConsoleApp/PostgreSQL/DatabaseInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.PostgreSQL
+{
+    public sealed class DatabaseInfo
+    {
+        public static string ConnString = "Host=localhost;Username=postgres;Password=password;Database=db";
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Npgsql;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
