5fa01965c96d1c670a58b92d378f4785ca3696c0
Johannes Tykesson <jochedator@gmail.com>
2023-02-06 20:37:03 +0100

    containerize the application with db

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,16 +2,22 @@
 using ConsoleApp.MappingTests;
 using Npgsql;
 
-namespace ConsoleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var benchmark = new MapperBenchmark();
-            benchmark.BenchmarkMappers();
-            benchmark.BenchmarkMappersOnlyMapping();
-            benchmark.BenchmarkMappersOnlyMappingEasyMapping();
-        }
-    }
-}
+// Mappers benchmarking below
+/*
+var benchmark = new MapperBenchmark();
+benchmark.BenchmarkMappers();
+benchmark.BenchmarkMappersOnlyMapping();
+benchmark.BenchmarkMappersOnlyMappingEasyMapping();
+*/
+
+
+// DB code below
+Console.WriteLine("Initiating db conn");
+var cs = "Host=db;Username=postgres;Password=pw;Database=testDb";
+using var con = new NpgsqlConnection(cs);
+con.Open();
+Console.WriteLine("Db initialized");
+var sql = "SELECT version()";
+using var cmd = new NpgsqlCommand(sql, con);
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {version}");
